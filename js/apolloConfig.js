c997d29b68fd489c4c64ad5ffed8cc1d225deea0
evgeny <sjs-master@yandex.ru>
2017-03-24 09:37:15 +0700

    configure apollo client datatype

--- js/apolloConfig.js
+++ js/apolloConfig.js
@@ -6,9 +6,47 @@ const networkInterface = createNetworkInterface({
     opts: {method: 'POST'}
 });
 
+const dataIdFromObject = (data) => {
+    switch (data.__typename) {
+        case 'User':
+            return `User#${data.uid}`;
+
+        case 'Event':
+            if (data.type && data.timestamp && data.entity ) {
+                const entity_id = dataIdFromObject(data.entity);
+                if (entity_id) {
+                    return `Event#${data.type}#${data.timestamp}#${entity_id}`;
+                }
+            }
+            break;
+
+        case 'EventMetadata':
+            if (data.event_key) {
+                return `EventMetadata#${data.event_key}`;
+            }
+            break;
+
+        case 'Timestamp':
+            if (data.timestamp) {
+                return `Timestamp#${data.timestamp}`;
+            }
+            break;
+    }
+
+    const id = data.id || data.tid;
+    if (!id) {
+        // console.log('undefined id', data);
+        return undefined;
+    }
+
+    return `${data.__typename}#${id}`;
+};
+
 const apolloClient = new ApolloClient({
     networkInterface,
-    // dataIdFromObject: o => `${o.__typename}:${o.id},`,
+    dataIdFromObject,
+    connectToDevTools: true,
+    addTypename: true,
     // shouldBatch: true,
 });
 
